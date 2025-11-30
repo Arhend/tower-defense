de29005f97c1c39360bd44eb9cf773a6b7596be6
Kyle Hendricks <khendricks7@protonmail.com>
2025-11-30 16:19:09 -0500

    Added forest assets

--- Scenes/Levels/base_level.gd
+++ Scenes/Levels/base_level.gd
@@ -1,3 +1,13 @@
 extends Node2D
 
+@export var show_no_build_tiles: bool = false
+
 @onready var enemy_pooling_service = $Services/EnemyPoolingService
+@onready var tower_no_build = $Tilemaps/TowerNoBuild
+
+
+func _ready():
+    if show_no_build_tiles:
+        tower_no_build.z_index = 1000
+    else:
+        tower_no_build.z_index = 0
